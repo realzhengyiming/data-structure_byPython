6c457afdae195d9e742426c019fa17b1c5f535c1
realzhengyiming <realzhengyiming@outlook.com>
2019-11-03 23:50:50 +0800

    增加排序算法

--- Sorts.py
+++ Sorts.py
@@ -29,7 +29,26 @@ def bubble_sort(lists):
     return lists
 
 
+def quick_sort(lists,left,right):
+    if left >= right:
+        return lists
+    key = lists[left]
+    low = left
+    high = right 
+    while left < right:
+        while left <right  and lists[right] >= key :
+            right-=1
+        lists[left] = lists[right]
+        while left <right and lists[left] <= key:
+            left += 1
+        lists[right] = lists[left]
+        quick_sort(lists,low,left-1)
+        quick_sort(lists,left+1,high)
+        return lists
+        
+
 
 print(insert_sort([2,3,4,1]))
 print(select_sort([2,3,4,1]))
 print(bubble_sort([2,3,4,1]))
+print(quick_sort([2,3,4,1],0,len([2,3,4,1])-1))
