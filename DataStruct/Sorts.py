bce6f7d14dae4d6f0e5c4219bdfd49b3f38c25c0
realzhengyiming <realzhengyiming@outlook.com>
2019-11-22 09:35:19 +0800

    增加自己的注解

--- DataStruct/Sorts.py
+++ DataStruct/Sorts.py
@@ -1,55 +1,76 @@
-# 都默认他们是升序，从小到大
-
-def insert_sort(lists):  # 简单插入排序
-    count = len(lists)
-    for i in range(1,count):
-        for j in range(i,0,-1):  # 倒叙的应该也是可以的
-            if lists[j-1]>lists[j]:
-                lists[j-1],lists[j] = lists[j],lists[j-1]
-    return lists
-
-
+#encoding:utf-8
 def select_sort(lists):
-    count = len(lists)
-    for i in range(0,count):   # 第一层确保逐个往后移动确认最小值
-        min = i  # 最开始，最左边的数字假装已经完成了排序，所以内层j从0+1开始
-        for j in range(i+1,count):    # 得到除左边排序成功后的部分的最小的值
-            if lists[min]>lists[j]:
+    min = 0
+    for i in range(0,len(lists)):
+        min = i
+        for j in range(i+1,len(lists)):
+            if lists[j]<lists[min]:
                 min = j
-        lists[i],lists[min] = lists[min],lists[i]   # 最小值与i交换
+        lists[min],lists[i] = lists[i],lists[min]
     return lists
 
 
 def bubble_sort(lists):
-    count = len(lists)
-    for i in range(0,count):
-        for j in range(count-1, i, -1):   # 从后面开始比到前面,下标和长度要区分开来
-            if lists[j] < lists[j-1]:
-                lists[j],lists[j-1] = lists[j-1],lists[j]
+    for i in range(0,len(lists)-1):  # 两两比较移动，n-1 次就可以，就好像分成n段，n-1 次就可以
+        for j in range(len(lists)-i-1):  # 注意：这儿需要减去一轮 ，这是为什么呢
+            if lists[j]>lists[j+1]:
+                lists[j],lists[j+1] = lists[j+1],lists[j]
     return lists
 
 
-def quick_sort(lists,left,right):
-    if left >= right:
+# 归并排序两部分
+def merge(left,right):
+    i,j = 0,0  # 初始化i,j
+    result = []
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i += 1    # 写while 的时候不要忘记了要给计数器迭代
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]  # 这儿直接把i后面未添加的直接加进来，以为已经排好序了
+    result += right[j:]
+    return result
+
+
+def merge_sort(lists):
+    if len(lists) <= 1:
         return lists
-    key = lists[left]
+    num = len(lists) // 2   # python3中需要使用// 来获得整数的商
+    left = merge_sort(lists[:num])
+    right = merge_sort(lists[num:])  # 这种就是递归的思想，不过左右边都一样的
+    result = merge(left,right)
+    return result
+
+
+# 下面是快速排序
+def quick_sort(lists,left,right):
+    if left>=right:
+        return lists
     low = left
-    high = right 
-    while left < right:
-        while left <right  and lists[right] >= key :
-            right-=1
-        lists[left] = lists[right]
-        while left <right and lists[left] <= key:
+    high = right
+    key = lists[left]  # 这个就是pivot，设定这个作为比较的枢轴 ，把值暂存
+    while left<right :
+        while left<right and lists[right] >= key:
+            right -= 1   # 不断的往前移动
+        lists[left] = lists[right]  # 把小的值给初始的left位置 
+        while left<right and lists[left] <= key:
             left += 1
-        lists[right] = lists[left]
-    lists[right] = key    
-    quick_sort(lists,low,left-1)
+        lists[right] = lists[left]   # 把这个大于中间值的数给右边
+    lists[right] = key  # 这个时候
+    quick_sort(lists,low,left-1)  # 把左右的
     quick_sort(lists,left+1,high)
     return lists
-        
 
 
-print(insert_sort([2,3,4,1]))
-print(select_sort([2,3,4,1]))
-print(bubble_sort([2,3,4,1]))
-print(quick_sort([2,3,4,1],0,len([2,3,4,1])-1))
+if __name__ == '__main__':
+    a = [6,4,3,7,8,2,1,1]
+    print(a)
+    print("排序结果")
+    print(select_sort(a))
+    print(bubble_sort(a))
+    print(merge_sort(a))
+    print(quick_sort(a,0,len(a)-1))
+
+    
